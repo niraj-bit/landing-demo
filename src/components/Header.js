9bdd4317c0cabcfd4009f096977fdb1e875f4d3a
Uday Mishra <udaymishra@Udays-MacBook-Pro.local>
2021-05-08 15:14:02 +0530

    menu shifted to right of the screen.

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto">
+            <ul className="navbar-nav ms-auto">
               <li className="nav-item active">
                 <a className="nav-link" href="#">
                   Home
